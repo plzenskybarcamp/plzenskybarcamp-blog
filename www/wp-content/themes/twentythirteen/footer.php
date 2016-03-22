60aef15eb36dcb67072b41edb0e8d2595a1900ff
Jakub Bouček <pan@jakubboucek.cz>
2016-03-22 07:39:31 +0100

    Versioning updates and template changes

--- www/wp-content/themes/twentythirteen/footer.php
+++ www/wp-content/themes/twentythirteen/footer.php
@@ -14,6 +14,20 @@
 		<footer id="colophon" class="site-footer" role="contentinfo">
 			<?php get_sidebar( 'main' ); ?>
 
+
+			<div class="action-panel">
+                		<p>&nbsp;</p>
+			</div>
+		        <div class="social-media">
+				<ul>
+					<li class="tw"><a href="https://twitter.com/plzenskybarcamp" target="_blank"><span data-hover="Twitter"><i>Twitter</i></span></a></li>
+					<li class="fb"><a href="https://www.facebook.com/plzenskybarcamp" target="_blank"><span data-hover="Facebook"><i>Facebook</i></span></a></li>
+					<li class="gplus"><a href="https://plus.google.com/+PlzenskyBarcampCZ" rel="publisher" target="_blank"><span data-hover="Google+"><i>Google+</i></span></a></li>
+					<li class="insta"><a href="https://instagram.com/plzenskybarcamp/" target="_blank"><span data-hover="Instagram"><i>Instagram</i></span></a></li>
+					<li class="yt"><a href="https://www.youtube.com/PlzenskyBarcamp" target="_blank"><span data-hover="Youtube"><i>Youtube</i></span></a></li>
+				</ul>
+			</div>
+
 			<div class="site-info">
 				<?php do_action( 'twentythirteen_credits' ); ?>
 				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentythirteen' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentythirteen' ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentythirteen' ), 'WordPress' ); ?></a>
